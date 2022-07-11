56e526a1c325c7d843eac6aab1f080fb62c3ecfb
Chan0305 <108973784+Chan0305@users.noreply.github.com>
2022-07-11 23:00:10 +0530

    Update Day1_Prob_2.c

--- DAY1/Day1_Prob_2.c
+++ DAY1/Day1_Prob_2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main() 
 {
-    char ch,a,e,i,o,u;
+    char ch,a,e,i,o,u,A,E,I,O,U;
     printf("Enter a character:");
     scanf("%c", &ch);
-    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    if(ch==('a'||'A')||ch=='e'||'E'||ch=='i'||'I'||ch=='o'||'O'||ch=='u'||'U')
 {
     printf("The character is vowel");
 }
