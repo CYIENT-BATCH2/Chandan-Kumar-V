5390f3b861ae7297c9a97dfa7aca0bfdfbf36251
Chan0305 <108973784+Chan0305@users.noreply.github.com>
2022-07-12 21:52:21 +0530

    Update Day1_Prob_2.c

--- DAY1/Day1_Prob_2.c
+++ DAY1/Day1_Prob_2.c
@@ -4,13 +4,13 @@ int main()
     char ch,a,e,i,o,u,A,E,I,O,U;
     printf("Enter a character:");
     scanf("%c", &ch);
-    if(ch==('a'||'A')||ch=='e'||'E'||ch=='i'||'I'||ch=='o'||'O'||ch=='u'||'U')
+    if(ch==('a'||'A')||ch==('e'||'E')||ch==('i'||'I')||ch==('o'||'O')||ch==('u'||'U'))
 {
-    printf("The character is vowel");
+    printf("The Entered character is vowel");
 }
     else
     {
-    printf("The character is Consonent");
+    printf("The Entered character is Consonent");
     }
     return 0;
 }
